7f3604421ea2fed771a24bec839b7a417808f94f
ruabelyan <58383424+ruabelyan@users.noreply.github.com>
2020-01-04 17:51:31 +0400

    4 jam ashxatanq

--- js/script.js
+++ js/script.js
@@ -1,7 +1,54 @@
-var arr = [1,5,9,7,66,3,4,25,9,41,1,7,66];
-var index0 
-for (let i = 0; i < arr.length; i++) {
  
+// var arr = [1,5,9,44,7,86,2,5,8,4,6,3,4,9,66,58,6,5];
+// var delArr = [];
+// var delArr2 = [];
 
+// var i = 0;
+// var g
+// var h
+
+//  while(i<arr.length){
+
+//     if(arr[i]% 2 == 0){
+//         g = arr[i]
+//         delArr.push(g)
+        
+//     }
+//     if(arr[i]% 2 !== 0){
+//         h = arr[i]
+//         delArr2.push(h)
+//         // console.log(delArr2)
+//     }
+   
+//   i++
+
+//  }
+//  console.log(delArr2)
+//  console.log(delArr)
+
+
+
+// var i = 0;
+
+// var arr = [1,5,9,44,7,86,2,5,8,4,6,3,4,9,66,58,6,5];
+// var max = arr[0]
+// while(i < arr.length){
+
+//     if(arr[i]>max){
+//         max = arr[i]
+        
+//     }
+
+//     i++
     
-}
+// }
+
+// console.log(max)
+
+
+// var arr = [1,5,9,44,7,86,2,5,8,4,6,3,4,9,66,58,6,5];
+//     for(key in arr){
+
+//     }
+
+
